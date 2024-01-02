e58ada723d3c7eb9cdfb8f8b44fb8e33283caedb
Haris Mauladi <haris1922.hmh@gmail.com>
2024-01-02 11:52:25 +0700

    fix : adjust width button

--- src/elements/ButtonForm.jsx
+++ src/elements/ButtonForm.jsx
@@ -8,7 +8,7 @@ function ButtonForm({ type, event, text }) {
       // eslint-disable-next-line react/button-has-type
       type={type}
       onClick={() => event()}
-      className="w-full bg-blue-500 font-bold rounded-md my-2 p-1 hover:bg-blue-800 text-xs sm:text-xl"
+      className="w-40 bg-blue-500 font-bold rounded-md my-2 p-0 hover:bg-blue-800 text-xs sm:text-xl"
     >
       {text}
     </button>
